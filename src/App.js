f47cc5598a3066b9a51187b2590d9db9bb47d7e3
valvandor <sincov.vitalii@gmail.com>
2021-12-07 20:08:09 +0300

    feat: create MUI theme
    simple MUI dark theme

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import './App.css';
 import MessageApp from './components/MessageApp';
+import { createTheme, ThemeProvider } from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    type: 'dark',
+    primary: {
+      main: '#e65100',
+    },
+    secondary: {
+      main: '#c62828',
+    },
+    error: {
+      main: '#d50000',
+    },
+    background: {
+      default: '#141414',
+      paper: '#424242',
+    },
+  },
+});
 
 function App() {
 
   return (
-    <div className="App">
-      <MessageApp />
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <MessageApp />
+      </div>
+    </ThemeProvider>
   );
 };
 
