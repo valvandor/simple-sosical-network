eb03b947c767d5642d95785177d19a0db9911593
valvandor <sincov.vitalii@gmail.com>
2021-12-09 13:31:05 +0300

    feat(urls): create base routing and nav-bar
    add some routes with stubs

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
 import './App.css';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
 import MessageApp from './components/MessageApp';
-import { ThemeProvider } from '@mui/material';
+import { AppBar, Button, ThemeProvider, Toolbar } from '@mui/material';
 import { theme } from './theme/themeMUI';
 
+
+function Home() {
+  return ( <h1>Home</h1> );
+}
+
+function Profile() {
+  return ( <h1>Profile</h1> );
+}
 
 function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <div className="App">
-        <MessageApp />
-      </div>
+      <BrowserRouter>
+        <AppBar position="static">
+          <Toolbar>
+            <Button to="/" component={Link}>Home</Button>
+            <Button to="/profile/" component={Link}>Profile</Button>
+            <Button to="/messages/" component={Link}>Messages</Button>
+          </Toolbar>
+        </AppBar>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/profile/" component={Profile} />
+          <Route exact path="/messages/">
+            <MessageApp parent="messages" />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </ThemeProvider>
   );
 };
