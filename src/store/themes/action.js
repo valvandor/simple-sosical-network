ea0ffb0ac923df8cf8022e5cbab01d1e9e761983
valvandor <sincov.vitalii@gmail.com>
2021-12-17 12:59:59 +0300

    doc(themes): describe action and selectors

--- src/store/themes/action.js
+++ src/store/themes/action.js
@@ -1,5 +1,10 @@
 export const CHANGE_THEME = 'THEME::CHANGE_THEME'
 
+/**
+ * 
+ * @param {number} newTheme 
+ * @returns 
+ */
 export const changeTheme = (newTheme) => ({
   type: CHANGE_THEME,
   payload: newTheme
