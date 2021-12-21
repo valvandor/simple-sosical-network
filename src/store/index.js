6c5d0e015f77ddd243589d21e11e6237f45926b9
valvandor <sincov.vitalii@gmail.com>
2021-12-21 15:15:48 +0300

    feat(store): add and use messages reducer
    implement action ADD_CHAT

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,14 @@
 import { createStore, combineReducers } from 'redux';
 
 import chatsReducer from "./chats/reducer"
+import messagesReducer from './messages/reducer'; 
 import { themeReducer } from "./themes/reducer";
 
 export const store = createStore(
   combineReducers({
     themes: themeReducer,
     chats: chatsReducer,
+    messages: messagesReducer,
   }),
   window.__REDUX_DEVTOOLS_EXTENSION__ &&
   window.__REDUX_DEVTOOLS_EXTENSION__()
